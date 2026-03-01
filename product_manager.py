f6284c4b9d7de6851b528e9d15ac3a5b7db9d21a
Ervin Ajdinovic <ervin_ajdinovic@hotmail.com>
2026-03-01 13:29:51 +0100

    Add remove_product_by_name method to ProductManager

--- product_manager.py
+++ product_manager.py
@@ -7,7 +7,14 @@ class ProductManager:
 
     def add_product(self, product):
         self.products.append(product)
-
+        
+    def remove_product_by_name(self, name):
+        for product in self.products:
+            if product.name.lower() == name.lower():
+                self.products.remove(product)
+                return True
+        return False
+    
     def display_all_products(self):
         if not self.products:
             print("No products available.")
